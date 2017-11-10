92e393215df51b53f8666924564f2ab0029aad88
Andrew Chambers <andrewchambers@fastmail.com>
2017-11-10 14:36:00 +1300

    improve documentation

--- cmd/wsemail/main.go
+++ cmd/wsemail/main.go
@@ -13,16 +13,34 @@ import (
 var (
 	generateAddress = flag.Bool("generate-address", false, "Generate a random fake email, print to stdout and exit")
 	apiToken        = flag.String("api-token", "", "API token to authenticate with, can also be specified with the env variable WEBSOCKETEMAIL_TOKEN")
-	fromAddress     = flag.String("from-address", "", "Subscribe to emails from this address")
+	fromAddress     = flag.String("for-address", "", "Subscribe to emails from this address")
 	numEmails       = flag.Int64("n", 1, "Wait for and print this many emails before exiting, less than or equal to zero waits forever")
 	timeoutSeconds  = flag.Uint64("timeout", 60, "Wait this many seconds for an email to arrive before giving up and terminating with an error, 0 for no timeout")
 )
 
 func usage() {
-	flag.Usage()
+	fmt.Fprintln(os.Stderr, "A tool for interacting with https://websocket.email")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	flag.PrintDefaults()
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "Examples:")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "  Generate a secure random email address:")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintf(os.Stderr, "    $ %s -generate-address\n", os.Args[0])
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "  Wait 10 seconds for a single email for john@websocket.email:")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintf(os.Stderr, "    $ %s -for-address john@websocket.email -timeout 10\n", os.Args[0])
+
+	os.Exit(1)
 }
 
 func main() {
+
+	flag.Usage = usage
+
 	flag.Parse()
 
 	if *generateAddress {
@@ -36,16 +54,14 @@ func main() {
 	}
 
 	if *fromAddress == "" {
-		_, _ = fmt.Fprintln(os.Stderr, "-from-address or -generate-address required\n")
-		usage()
+		_, _ = fmt.Fprintln(os.Stderr, "-from-address or -generate-address required, see -help for details")
 		os.Exit(1)
 	}
 
 	if *apiToken == "" {
 		*apiToken = os.Getenv("WEBSOCKETEMAIL_TOKEN")
 		if *apiToken == "" {
-			_, _ = fmt.Fprintln(os.Stderr, "-api-token or env variable WEBSOCKETEMAIL_TOKEN required\n")
-			usage()
+			_, _ = fmt.Fprintln(os.Stderr, "-api-token or env variable WEBSOCKETEMAIL_TOKEN required, see -help for details")
 			os.Exit(1)
 		}
 	}
